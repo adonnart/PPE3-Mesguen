654c571ab83ae908daa51bb34fa4bc605d270f9c
15marosm <mmaroswork@gmail.com>
2017-03-06 14:44:35 +0100

    Code de redemarage Oracle ADD

--- Db_V_Mesguen.sql
+++ Db_V_Mesguen.sql
@@ -118,6 +118,19 @@ REFERENCES Clients(IdClient);
 
 
 
+/* Redemarer Oracle */
+@echo off
+echo shutdown immediate>"oracle.sql"
+echo. startup open>>"oracle.sql"
+echo. exit>>"oracle.sql"
+
+sqlplus system/estran as sysdba @oracle.sql
+
+del /s oracle.sql
+cls
+
+echo Operation reussie
+pause
 
 
 
