698f102710453a5b3cd8987b44c2a4254c83a682
adonnart <alexandre.donnart@gmail.com>
2017-04-28 09:19:05 +0200

    Update modifClient.vb

--- modifClient.vb
+++ modifClient.vb
@@ -8,13 +8,10 @@ Public Class modifClient
     Dim connString As String
     Dim donnee As DataTable
 
-    'connexion à la base de donnees
+    'Connexion à la base de donnees'
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
         connString = "Driver={Microsoft ODBC for Oracle};CONNECTSTRING=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.220.18)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORA18)));Uid=SCOTT;Pwd=estran;"
-
         myConnection.ConnectionString = connString
-
         Try
             myConnection.Open()
         Catch ex As Odbc.OdbcException
@@ -22,9 +19,9 @@ Public Class modifClient
         End Try
     End Sub
 
-    'affichage des noms de client
-    Private Sub afficheNomPdtModif_Click(sender As System.Object, e As System.EventArgs) Handles afficheNomPdtModif.Click
-        Dim query As String = "SELECT ID, NOMCLIENT FROM CLIENT"
+    'Affichage des noms des clients'
+    Private Sub afficheNomCltModif_Click(sender As System.Object, e As System.EventArgs) Handles afficheNomCltModif.Click
+        Dim query As String = "SELECT idCli, nomCli FROM CLIENT"
         donnee = New DataTable
         myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
         myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
@@ -32,35 +29,36 @@ Public Class modifClient
         nomPdtModif.DataSource = donnee
     End Sub
 
-    Private Sub enregistrerPdtModif_Click(sender As System.Object, e As System.EventArgs) Handles enregistrerPdtModif.Click
+    'Enregistrement du client
+    Private Sub enregistrerCltModif_Click(sender As System.Object, e As System.EventArgs) Handles enregistrerCltModif.Click
         Dim response As MsgBoxResult
         response = MsgBox("Voulez vous confirmer la mise à jour des infos client ?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Validation")
         If response = MsgBoxResult.Yes Then
-            Dim cltId As Integer = nomCltModif.CurrentCell.Value
-            Dim adrClt As String = adrCltModif.Text
-            Dim descClt As String = descCltModif.Text
+            Dim idCli As Integer = nomCliModif.CurrentCell.Value
+            Dim adrCli As String = adrCliModif.Text
+            Dim descCli As String = descCliModif.Text
             Dim info As String = "Résumé des modifications : " & vbCrLf
-            info += "n° de client : " & cltId & vbCrLf & "adresse client :" & vbCrLf & adrClt & vbCrLf & "description du client : " & vbCrLf & descClt & vbCrLf
-
-            info = "UPDATE CLIENT SET ADRCLIENT='" & adrClt & "', DESCRIPTIONCLIENT='" & descClt & "' WHERE ID='" & cltId & "' "
+            
+            info += "N° : " & idCli & vbCrLf & "Nom :" & nomCli & vbCrLf & "Adresse :" & vbCrLf & adrCli & vbCrLf & "Description : " & vbCrLf & descClt & vbCrLf
+            info = "UPDATE CLIENT SET ADRCLIENT='" & adrCli & "', DESCRIPTIONCLIENT='" & descCli & "' WHERE ID='" & idCli & "' "
+            
             MsgBox(info)
+            
             Dim da As New Odbc.OdbcDataAdapter
-
             Dim cmd As New Odbc.OdbcCommand(info, myConnection)
 
             da.UpdateCommand = cmd
             da.UpdateCommand.ExecuteNonQuery()
-
         ElseIf response = MsgBoxResult.No Then
             Exit Sub
         End If
     End Sub
 
-    'annulation du form
-    Private Sub AnnulerCommande_Click(sender As System.Object, e As System.EventArgs) Handles annulePdtModif.Click
-        'annuler et revenir au choix de l'action, ferme la fenetre en version solo
+    'Annulation du formulaire'
+    Private Sub AnnulerCommande_Click(sender As System.Object, e As System.EventArgs) Handles annuleCltModif.Click
+        'Annuler et revenir au choix de l'action, ferme la fenetre en version solo'
         Dim response As MsgBoxResult
-        response = MsgBox("Voulez vous annuler la modifications des informations?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Valider")
+            response = MsgBox("Voulez-vous annuler la modification des informations ?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Valider")
         If response = MsgBoxResult.Yes Then
             Me.Show()
             Me.Close()
@@ -69,15 +67,16 @@ Public Class modifClient
         End If
     End Sub
 
-    Private Sub descPdtModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles descPdtModif.TextChanged
+    Private Sub nomCltModif_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles nomCltModif.CellContentClick
+
+    End Sub
+    
+    Private Sub adrCltModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles adrCltModif.TextChanged
+
+    End Sub
+    
+    Private Sub descCltModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles descCltModif.TextChanged
 
     End Sub
 
-    Private Sub codePdtModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles codePdtModif.TextChanged
-
-    End Sub
-
-    Private Sub nomPdtModif_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles nomPdtModif.CellContentClick
-
-    End Sub
 End Class
