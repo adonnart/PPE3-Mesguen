8027b0581b59f75daa0da01f48a84f8613fb5ba4
JulienHellio <j-hellio@outlook.fr>
2017-05-18 10:52:04 +0200

    Update CU04_modif_produit.vb

--- TP10 WINFORM/TP10 WINFORM/CU04_modif_produit.vb
+++ TP10 WINFORM/TP10 WINFORM/CU04_modif_produit.vb
@@ -1,5 +1,4 @@
-﻿Public Class CU04_modif_produit
-
+Public Class modifProduit
     Dim myConnection As New Odbc.OdbcConnection
     Dim myCommand As New Odbc.OdbcCommand
     Dim myInsert As New Odbc.OdbcCommand
@@ -9,9 +8,11 @@
     Dim connString As String
     Dim donnee As DataTable
 
-    Private Sub CU04_modif_produit_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    'connexion à la base de donnees
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'connString = "Driver={Microsoft ODBC for Oracle};CONNECTSTRING=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.220.18)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORA18)));Uid=SCOTT;Pwd=estran;"
 
-        connString = "Driver={Microsoft ODBC for Oracle};CONNECTSTRING=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.23.80)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl)));Uid=mesguen3;Pwd=Estran;"
+        connString = "Driver={Microsoft ODBC for Oracle};CONNECTSTRING=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.220.217)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ORA17)));Uid=Matt;Pwd=estran;"
 
         myConnection.ConnectionString = connString
 
@@ -21,56 +22,80 @@
             MessageBox.Show(ex.Message)
         End Try
 
-        Dim query As String = "SELECT PRODUITID, NOMPRODUIT FROM PRODUIT"
+        Dim query As String = "SELECT PRODUITID, NOMPRODUIT,CODEPRODUIT FROM PRODUIT"
         donnee = New DataTable
         myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
         myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
         myAdapter.Fill(donnee)
         nomPdtModif.DataSource = donnee
 
-        nomPdtModif.SelectionMode = DataGridViewSelectionMode.FullRowSelect
-
     End Sub
+
 
     Private Sub enregistrerPdtModif_Click(sender As System.Object, e As System.EventArgs) Handles enregistrerPdtModif.Click
 
-        Dim pdtId As Integer = nomPdtModif.SelectedRows(0).Cells(0).Value.ToString
-        Dim codePdt As String = codePdtModif.Text
-        Dim commPdt As String = commPdtModif.Text
-
-        Dim req As String = "UPDATE PRODUIT SET CODEPRODUIT = '" & codePdt & "', COMMENTAIREPRODUIT = '" & commPdt & "' WHERE PRODUITID = '" & pdtId & "' "
-
         Dim reponse As MsgBoxResult
-        Dim da As New Odbc.OdbcDataAdapter
-        Dim cmd As New Odbc.OdbcCommand(req, myConnection)
+        
+            Dim pdtId As Integer = nomPdtModif.CurrentCell.Value
+            Dim codePdt As String = codePdtModif.Text
+            Dim descPdt As String = descPdtModif.Text
+            Dim info As String = "Résumé des modifications : " & vbCrLf
+            info += "n° de produit : " & pdtId & vbCrLf & "code produit :" & vbCrLf & codePdt & vbCrLf & "description du produit : " & vbCrLf & descPdt & vbCrLf
 
-        da.UpdateCommand = cmd
-        da.UpdateCommand.ExecuteNonQuery()
+        info = "UPDATE PRODUIT SET CODEPRODUIT='" & codePdt & "', COMMENTAIREPRODUIT='" & descPdt & "' WHERE PRODUITID='" & pdtId & "' "
 
-        reponse = MsgBox("Mise à jour des informations effectuée." & vbCrLf & "Voulez-vous modifier d'autres produits ?", MsgBoxStyle.Question + MsgBoxStyle.YesNo)
+            Dim da As New Odbc.OdbcDataAdapter
+
+            Dim cmd As New Odbc.OdbcCommand(info, myConnection)
+
+
+            da.UpdateCommand = cmd
+            da.UpdateCommand.ExecuteNonQuery()
+        reponse = MsgBox("Mise à jour des informations produit effectuée!" & vbCrLf & " Voulez-vous modifier d'autres produits?", MsgBoxStyle.Question + MsgBoxStyle.YesNo)
 
         If reponse = MsgBoxResult.Yes Then
-            Me.codePdtModif.Text = Me.commPdtModif.Text = ""
+            Me.codePdtModif.Text = ""
+            Me.descPdtModif.Text = ""
         ElseIf reponse = MsgBoxResult.No Then
             Dim choix As New CU02_choix
             choix.Show()
             Me.Close()
-        End If
 
+        End If
+        
     End Sub
 
-    Private Sub AnnulerModif_Click(sender As System.Object, e As System.EventArgs) Handles annulePdtModif.Click
 
+
+
+    'annulation du form
+    Private Sub AnnulerModif_Click(sender As System.Object, e As System.EventArgs) Handles annulePdtModif.Click
+        'annuler et revenir au choix de l'action, ferme la fenetre en version solo
         Dim response As MsgBoxResult
-        response = MsgBox("Voulez-vous annuler la modification des informations ?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Valider")
+        response = MsgBox("Voulez vous annuler la modifications des informations?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "Valider")
         If response = MsgBoxResult.Yes Then
+            Me.Show()
+            Me.Close()
+        ElseIf response = MsgBoxResult.No Then
             Dim choix As New CU02_choix
             choix.Show()
             Me.Close()
-        ElseIf response = MsgBoxResult.No Then
-            Me.Show()
         End If
+    End Sub
+
+    Private Sub descPdtModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles descPdtModif.TextChanged
 
     End Sub
+
+    Private Sub codePdtModif_TextChanged(sender As System.Object, e As System.EventArgs) Handles codePdtModif.TextChanged
+
+    End Sub
+
+    Private Sub nomPdtModif_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles nomPdtModif.CellContentClick
+
+
+
+    End Sub
+
 
 End Class
